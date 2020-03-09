f44dda344f053c4a5f196cccfa0951b2e1575551
Владислав Пономарёв <Git@PonomareVlad.ru>
2020-03-09 06:53:06 +0500

    Banner prototype (#1)
    * Test banner
    
    * Updated styles
    
    * Updated meta
    
    * Added adaptive styles
    
    * Added condition for banner render

--- index.js
+++ index.js
@@ -1,3 +1,70 @@
-function Love(){
-  return console.info('with ❤️ from https://Ponomarev.Studio');
+class PS {
+    constructor() {
+        return new URLSearchParams(location.search).has('PS') ?
+            PS.preRenderAssets().then(PS.renderFloatBanner) : null;
+    }
+
+    static async preRenderAssets() {
+        const images = [
+            // 'https://ponomarev.studio/images/logo/Mono.svg',
+            'https://ponomarev.studio/images/logo/Gradient.128px.png'
+        ];
+        images.forEach(image => new Image().src = image);
+    }
+
+    static async renderFloatBanner() {
+        const bannerNode = document.createElement('a');
+        bannerNode.title = 'Разработано Ponomarev Studio 🚀';
+        bannerNode.href = 'https://Ponomarev.Studio';
+        bannerNode.target = '_blank';
+        bannerNode.id = 'psBanner';
+        bannerNode.innerHTML = `<style>
+        #psBanner {
+            --size: 60px;
+            --margin: 20px;
+            --filter: blur(35px);
+            -webkit-backdrop-filter: var(--filter);
+            backdrop-filter: var(--filter);
+            background-color: rgba(255, 255, 255, .05);
+            z-index: 2019;
+            position: fixed;
+            left: var(--margin);
+            bottom: var(--margin);
+            transition: all .3s;
+        }
+
+        #psBanner:after {
+            content: ' ';
+            background-image: url('https://ponomarev.studio/images/logo/Gradient.128px.png');
+            position: absolute;
+            opacity: 0;
+            transition: opacity .5s;
+        }
+
+        #psBanner,
+        #psBanner:after {
+            -webkit-mask-image: urL('https://ponomarev.studio/images/logo/Mono.svg');
+            -webkit-mask-size: contain;
+            -webkit-mask-position: center;
+            -webkit-mask-repeat: no-repeat;
+            background-size: 100%;
+            background-position: center;
+            background-repeat: no-repeat;
+            width: var(--size);
+            height: calc(var(--size) / 2);
+        }
+
+        #psBanner:hover:after {
+            opacity: 1;
+        }
+        @media (max-width: 1024px) {
+            #psBanner{
+                --size: 40px;
+            }
+        }
+    </style>`;
+        document.body.appendChild(bannerNode);
+    };
 }
+
+new PS();
